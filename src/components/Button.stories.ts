37ed9d27b7a273263f3e819f410d2002ccf25f29
usmangq12 <usmangq12@gmail.com>
2025-05-22 19:56:54 +0500

    feat:updated code

---
+++ src/components/Button.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta = {
+    title : 'Button',
+    component : Button,
+    tags: ['autodocs'],
+} satisfies Meta<typeof Button>
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary : Story ={
+    args : {
+        label :"Primary"
+    }
+}
+
+export const Secondary : Story ={
+    args : {
+        label :"Secondary"
+    }
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+
+export const Button = ({label} : {label : string}) => {
+  return (
+    <div className='flex justify-center items-center h-12 w-12 bg-amber-600 px-8 py-8 rounded-xl'>
+     {label}
+    </div>
+  )
+}
+
+
+
+
