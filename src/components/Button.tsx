16f1fe267d5a531b14c0aab6410bb87a97256a75
usmangq12 <usmangq12@gmail.com>
2025-05-22 20:10:39 +0500

    fix:remove rounded

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,7 @@
-
-export const Button = ({label} : {label : string}) => {
+export const Button = ({ label }: { label: string }) => {
   return (
-    <div className='flex justify-center items-center h-12 w-12 bg-amber-600 px-8 py-8 rounded-xl'>
-     {label}
+    <div className="flex justify-center items-center h-12 w-12 bg-amber-600 px-8 py-8 rounded">
+      {label}
     </div>
-  )
-}
-
-
-
-
+  );
+};
